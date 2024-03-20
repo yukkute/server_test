4558a7b123b6413d57334bff683ae3a3df705a87
yukkute <yukkute@protonmail.com>
2024-03-20 22:50:55 +0300

    worked on android

--- frb_init.sh
+++ frb_init.sh
@@ -7,22 +7,22 @@ ln -s "$(readlink -e "$source_dir")" "$target_dir"
 
 cd client
 
-REPO_URL="https://github.com/fzyzcjy/flutter_rust_bridge.git"
-FOLDER_PATH="frb_utils"
-
-if [ ! -d "$FOLDER_PATH" ]; then
-    git clone --filter=blob:none $REPO_URL temp_repo
-    cd temp_repo
-    git config core.sparseCheckout true
-    echo "$FOLDER_PATH/*" > .git/info/sparse-checkout
-    git pull origin master
-    mv $FOLDER_PATH ..
-    cd ..
-    rm -rf temp_repo
-fi
-
-SHADY_PUBSPEC="frb_utils/pubspec.yaml"
-sed -i -e ':a;N;$!ba;s|flutter_rust_bridge:\n    path: ../frb_dart|flutter_rust_bridge: ^2.0.0-dev.28|g' $SHADY_PUBSPEC
-sed -i 's/test_core: \^0.5.9/^0.6.0/' $SHADY_PUBSPEC
+# REPO_URL="https://github.com/fzyzcjy/flutter_rust_bridge.git"
+# FOLDER_PATH="frb_utils"
+#
+# if [ ! -d "$FOLDER_PATH" ]; then
+#     git clone --filter=blob:none $REPO_URL temp_repo
+#     cd temp_repo
+#     git config core.sparseCheckout true
+#     echo "$FOLDER_PATH/*" > .git/info/sparse-checkout
+#     git pull origin master
+#     mv $FOLDER_PATH ..
+#     cd ..
+#     rm -rf temp_repo
+# fi
+#
+#SHADY_PUBSPEC="frb_utils/pubspec.yaml"
+#sed -i -e ':a;N;$!ba;s|flutter_rust_bridge:\n    path: ../frb_dart|flutter_rust_bridge: ^2.0.0-dev.28|g' $SHADY_PUBSPEC
+#sed -i 's/test_core: \^0.5.9/^0.6.0/' $SHADY_PUBSPEC
 
 echo "finished" $(basename "$0")
