58fffe7d6c5870ef935fc6045734b3698062b7ee
yukkute <yukkute@protonmail.com>
2024-10-10 17:58:35 +0300

    nix

--- flake.nix
+++ flake.nix
@@ -1,18 +1,33 @@
 {
   description = "Minimal Nix flake with Flutter, Rust, and protoc";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-  outputs = { self, nixpkgs }: {
-    devShells.default = import nixpkgs {
-      system = "x86_64-linux";
-    } .mkShell {
-      buildInputs = with pkgs; [
-        flutter
-        rustc
-        cargo
-        protobuf
-      ];
-    };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = false;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            #dart
+            rustup
+            protobuf
+          ];
+          
+          shellHook = ''
+            #export PATH=$PATH:${pkgs.dart}/bin
+            export PATH=$PATH:${pkgs.rustup}/bin
+            export PATH=$PATH:${pkgs.protobuf}/bin
+            echo "Development environment ready!"
+          '';
+        };
+      });
 }
